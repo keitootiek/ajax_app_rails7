01d13afef7598e998ddccd770bb814c524ac2c51
keitootiek <137137801+keitootiek@users.noreply.github.com>
2023-07-07 19:24:39 +0900

    JSでレスポンスを処理する

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,14 +1,9 @@
-// import { Application } from "@hotwired/stimulus"
+//  import { Application } from "@hotwired/stimulus"
 
 // const application = Application.start()
 
-// // Configure Stimulus development experience
+//  //Configure Stimulus development experience
 // application.debug = false
 // window.Stimulus   = application
 
 // export { application }
-
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "memo"
